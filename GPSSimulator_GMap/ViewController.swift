3932fa2b60f5d709fbaf964a7c670c6970321b27
Jaromir Siska <siskaj@me.com>
2015-02-26 18:40:36 +0100

    Initial Commit

---
+++ GPSSimulator_GMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSSimulator_GMap
+//
+//  Created by Jaromir on 26.02.15.
+//  Copyright (c) 2015 Baltoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
