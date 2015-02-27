61489aa7f904f3318f52484af681e68f0055738c
Jaromir Siska <siskaj@me.com>
2015-02-27 07:02:17 +0100

    Pridani Frameworku GPX

---
+++ GPX/GPX.h
@@ -0,0 +1,38 @@
+//
+//  GPX.h
+//  GPX
+//
+//  Created by Jaromir on 27.02.15.
+//  Copyright (c) 2015 Baltoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GPX.
+FOUNDATION_EXPORT double GPXVersionNumber;
+
+//! Project version string for GPX.
+FOUNDATION_EXPORT const unsigned char GPXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPX/PublicHeader.h>
+
+#import <GPX/GPXParser.h>
+#import <GPX/GPXConst.h>
+#import <GPX/GPXType.h>
+#import <GPX/GPXRoot.h>
+#import <GPX/GPXMetadata.h>
+#import <GPX/GPXWaypoint.h>
+#import <GPX/GPXRoutePoint.h>
+#import <GPX/GPXTrackPoint.h>
+#import <GPX/GPXRoute.h>
+#import <GPX/GPXTrack.h>
+#import <GPX/GPXExtensions.h>
+#import <GPX/GPXTrackSegment.h>
+#import <GPX/GPXCopyright.h>
+#import <GPX/GPXLink.h>
+#import <GPX/GPXEmail.h>
+#import <GPX/GPXPerson.h>
+#import <GPX/GPXAuthor.h>
+#import <GPX/GPXPoint.h>
+#import <GPX/GPXPointSegment.h>
+#import <GPX/GPXBounds.h>
